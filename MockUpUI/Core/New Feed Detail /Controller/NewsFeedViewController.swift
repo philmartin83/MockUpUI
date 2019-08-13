6e40e8fa44b4df6894afd74a791e425e5aa0f3aa
Phil Martin <phil.martin1983@gmail.com>
2019-08-13 19:25:55 +0100

    Added news feed viewcontroller

---
+++ MockUpUI/Core/New Feed Detail /Controller/NewsFeedViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NewsFeedViewController.swift
+//  MockUpUI
+//
+//  Created by Philip Martin on 13/08/2019.
+//  Copyright © 2019 Phil Martin. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+    
+    let presenter = NewsFeedPresenter()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        presenter.controller = self
+    }
+    
+
+}
---
+++ MockUpUI/Core/New Feed Detail /Presenter/NewsFeedPresenter.swift
@@ -0,0 +1,23 @@
+//
+//  NewsFeedPresenter.swift
+//  MockUpUI
+//
+//  Created by Philip Martin on 13/08/2019.
+//  Copyright © 2019 Phil Martin. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedPresenter: UIView, LayoutProtocol, NavigationBarProtocol{
+    
+    weak var controller: NewsFeedViewController?
+    
+    func setupNavBar() {
+        
+    }
+    
+    func displayLayout() {
+        
+    }
+    
+}
